d06fbea4740422874964c845fe8958434e3a19d4
Hanif Carroll <hanifcarroll@gmail.com>
2024-08-29 11:14:04 -0300

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Hanif Carroll - Full Stack Software Developer';
+export const SITE_TAGLINE = 'Improve. Create. Connect.'; // site tagline
+export const SITE_DESCRIPTION = 'Personal website for Hanif Carroll, showcasing writing, photography, and projects.'; // site meta description for SEO
